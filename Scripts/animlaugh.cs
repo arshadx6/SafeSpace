b8b777093c1dc946e6030e1ed61ccb922384b5ab
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2021-04-13 13:52:10 +0530

    Add files via upload

---
+++ Scripts/animlaugh.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animlaugh : MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource aud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if((Input.GetTouch(0).phase == TouchPhase.Began))
+        { 
+        	Ray ray=Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        	RaycastHit hit;
+        	if(Physics.Raycast(ray,out hit)){
+                if(hit.transform.name=="laugh"){
+                    animator.Play("animate");
+                    aud.Play();
+                }
+                
+        	}
+        }
+    }
+}
